2b1337323f5b85eeebf1b3b7ccd3e25dd2f51e04
Aaditya Bhattarai <30322868+Aaditya-327@users.noreply.github.com>
2018-06-18 00:42:51 +0545

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,15 @@
 import os 
+import sys
+
 # text = os.environ.get('MESSAGE', None)
 is_ascii = lambda s: len(s) == len(s.encode())
+main = lambda: print('yes done')
 
-text = input('Enter the MESSAGE: \n')
+if len(sys.argv) >1:
+	text = sys.argv[1]
+else:
+	text = input('Enter the MESSAGE: \n')
+
 def can_send(text):
 	if type(text) != type(''):
 		return False, 'Not text'
@@ -14,3 +21,8 @@ def can_send(text):
 		return is_ascii(text), text
 
 print(can_send(text))
+finish = input('Now do i send the message?')
+if finish == 'y':
+	main()
+else:
+	print('fail')
